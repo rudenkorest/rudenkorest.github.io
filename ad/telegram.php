457ceb1d53697eda4194012658de32c000a4b9ed
rudenkorest <35296155+rudenkorest@users.noreply.github.com>
2018-06-18 14:38:10 +0300

    Add files via upload

---
+++ ad/telegram.php
@@ -0,0 +1,28 @@
+<?php
+
+/* https://api.telegram.org/bot494412368:AAE5EQq7RjHIAErah15ZEMMGqhnQaS-_7a4/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
+$token = "494412368:AAE5EQq7RjHIAErah15ZEMMGqhnQaS-_7a4";
+$chat_id = "-250704760";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Email' => $email
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
